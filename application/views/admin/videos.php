e9602d35d0e228b166ce4b4383323dafc98146ab
Forhan Noor <forhannoor@gmail.com>
2015-06-27 18:38:43 +0600

    admin/videos video url fixed

--- application/views/admin/videos.php
+++ application/views/admin/videos.php
@@ -3,11 +3,11 @@
 <?php foreach($videos as $video): ?>
     <?php $thumbnail = array('src' => 'assets/images/thumbnail.jpg', 'title' => $video['title']) ?>
     <li>
-        <?php echo anchor('topmenu/video/'.$video['name'], img($thumbnail), array('target' => '_blank')) ?>
+        <?php echo anchor('topmenu/video/' . urlsafe_b64encode($video['name']), img($thumbnail), array('target' => '_blank')) ?>
         <br />
         <center>
         <p>
-        <?php echo anchor('topmenu/video/'.$video['name'], '<i class = \'icon-play\'></i>Play', array('target' => '_blank')) ?>
+        <?php echo anchor('topmenu/video/' . urlsafe_b64encode($video['name']), '<i class = \'icon-play\'></i>Play', array('target' => '_blank')) ?>
         &nbsp;&nbsp;
         <?php echo anchor('admin/video_delete/' . $video['id'], '<i class = \'icon-remove\'></i>Delete', array('onclick' => "return confirm('You are about to delete a record. This cannot be undone. Are you sure?')")) ?>
         </p>
