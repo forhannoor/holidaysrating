6dfad9299310961d126fc7eb14c29c66e875036b
Forhan Noor <forhannoor@gmail.com>
2015-07-10 22:02:26 +0600

    video url in user/index and user/show_video/all modified

--- application/views/user/show_video_all.php
+++ application/views/user/show_video_all.php
@@ -1,6 +1,6 @@
 <?php foreach($videos as $video): ?>
 <?php $thumbnail = array('src' => 'assets/images/thumbnail.jpg', 'title' => $video->title, 'width' => '104', 'height' => '80') ?>
-<?php echo anchor('topmenu/video/'.$video->name, img($thumbnail)) ?>
+<?php echo anchor('topmenu/video/' . urlsafe_b64encode($video->name), img($thumbnail)) ?>
 <?php echo hspace(2) ?>
 <?php endforeach ?>
 <br />
